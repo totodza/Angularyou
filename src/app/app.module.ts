6fcd73d0f89e9aad889cef55d8d8a1c5d0a02d4d
totodza <dazaantonio34@gmail.com>
2020-07-13 22:16:38 -0500

    Directiva ngif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
